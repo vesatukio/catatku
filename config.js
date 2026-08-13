fe7c71386b707cfdf13b032464648861d3052e78
led <vesatukio@gmail.com>
2026-08-13 14:18:17 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const API_URL = 'https://script.google.com/macros/s/AKfycbyW1qBf0LX3qPtSwRrvaxIuiIcHMFkuy9PViZ-YZrTfZeM6WqCNlfcNJN1CVzE7M1F3/exec';
+const API_KEY = 'CATATKU-2026-PRIBADI';
+
+let DATA = {
+  dashboard: null,
+  history: [],
+  barang: [],
+  penjualan: [],
+  belanja: [],
+  hutang: [],
+  kategori: [],
+  notifikasi: []
+};
